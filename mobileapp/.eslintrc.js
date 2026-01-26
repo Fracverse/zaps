0a911acae76dbe9d32e4978d3ae424174416206d
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-01-26 21:39:17 +0100

    chore: fix failed tests, build and fmt

--- mobileapp/.eslintrc.js
+++ mobileapp/.eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['react', 'react-hooks'],
-  parser: '@typescript-eslint/parser',
+  extends: ["expo", "prettier"],
+  plugins: ["react", "react-hooks"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.expo/'],
+  ignorePatterns: ["node_modules/", "dist/", ".expo/"],
 };
